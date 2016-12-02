252cf1d4d5f17ceeec69440b890b13b48a8977d4
Yu-Hua Chang <yuhua@taiwankk.com>
2016-12-02 08:32:10 +0800

    init commit

---
+++ src/main/java/com/taiwankk/rfid/tags/RfidTagsApplication.java
@@ -0,0 +1,12 @@
+package com.taiwankk.rfid.tags;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfidTagsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RfidTagsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/taiwankk/rfid/tags/RfidTagsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.taiwankk.rfid.tags;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RfidTagsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
